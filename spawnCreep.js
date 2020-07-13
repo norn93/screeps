21d7469e167c00ae32a9ffc2af4a8741fcb842e5
George Knowlden <george.knowlden@gmail.com>
2020-07-13 17:32:22 +0800

    More dry run code for spawner

--- spawnCreep.js
+++ spawnCreep.js
@@ -1,9 +1,22 @@
-function spawnCreep(role, availableEnergy) {
+function spawnCreep(role, work, carry, move) {
     console.log("Spawning a new creep with role:", role, "and", availableEnergy, "energy available.");
     var roles = ["builder", "harvester", "freight", "upgrader"];
     var valid_role = roles.includes(role);
     if (valid_role) {
         console.log(role, "is a valid role!");
+        var parts = [];
+        for (var i = 0; i <= work; i++) {
+            parts.push(WORK)
+        }
+        for (var i = 0; i <= carry; i++) {
+            parts.push(CARRY)
+        }
+        for (var i = 0; i <= move; i++) {
+            parts.push(MOVE)
+        }
+        console.log("Parts:", parts);
+        var name = role + Game.time;
+        console.log("Name:", name);
     } else {
         console.log(role, "is not a valid role");
     }
