360edf37eff71d957c8f14d889306d53570a527a
George Knowlden <george.knowlden@gmail.com>
2020-07-15 00:00:17 +0800

    Finding nearest before attacking

--- role.defender.js
+++ role.defender.js
@@ -27,6 +27,9 @@ var roleDefender = {
                 if(creep.moveTo(rampart) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(rampart, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
+                // Find targets to attack
+                nearest_hostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                
                 creep.attack(nearest_hostile);
             } else {
                 // Find targets to attack
