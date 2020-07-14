67b735b1e47c7c0f83a9b5eaa62897db21b5d819
George Knowlden <george.knowlden@gmail.com>
2020-07-14 23:21:13 +0800

    testing creep recycle

--- role.defender.js
+++ role.defender.js
@@ -15,42 +15,42 @@ var roleDefender = {
 
         if(creep.memory.defending) {
             // Find targets to attack
-            // Find the creep with the most healing parts
-            // var hostiles = Game.spawns['Spawn1'].room.find(FIND_HOSTILE_CREEPS);
-            // var max_healing_parts = 0;
-            // var worst_hostile = hostiles[0];
-            // for(var i in hostiles) {
-            //     var hostile = hostiles[i];
-            //     var healing_parts = 0;
-            //     var body_contents = hostile.body;
-            //     for (var body_element in body_contents) {
-            //         var this_part = body_contents[body_element]["type"]
-            //         if (this_part == "heal") {
-            //             healing_parts += 1;
-            //         }
-            //     }
-            //     if (healing_parts > max_healing_parts) {
-            //         worst_hostile = hostile;
-            //     }
-            // }
-            worst_hostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            nearest_hostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 
             // Go to them
             // Attack them
-            if(creep.attack(worst_hostile) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(worst_hostile, {visualizePathStyle: {stroke: '#ffaa00'}});
+            if(creep.attack(nearest_hostile) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(nearest_hostile, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         } else {
-            // Go to a rampart
-            var rampart = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (
-                        structure.structureType == STRUCTURE_RAMPART
-                    );
+            if (creep.ticksToLive > 200) {
+                // Go to a rampart
+                var rampart = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (
+                            structure.structureType == STRUCTURE_RAMPART
+                        );
+                    }
+                });
+                if(creep.moveTo(rampart) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(rampart, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
-            });
-            if(creep.moveTo(rampart) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(rampart, {visualizePathStyle: {stroke: '#ffffff'}});
+            } else {
+                // Time to die
+                // Go to a container
+                var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (
+                            structure.structureType == STRUCTURE_CONTAINER
+                        );
+                    }
+                });
+                if(creep.moveTo(container) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(container, {visualizePathStyle: {stroke: '#ffffff'}});
+                } else {
+                    Game.spawns['Spawn1'].recycleCreep(creep);
+                }
+
             }
         }
     }
