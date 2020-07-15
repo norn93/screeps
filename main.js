9cd77c40975d538209e2f6d2c77a46ddb8f91225
George Knowlden <george.knowlden@gmail.com>
2020-07-15 15:15:52 +0800

    Added some notes

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ module.exports.loop = function () {
 
     console.log("TODO: Move energy between links");
     console.log("TODO: Make link upgrader");
-    console.log("TODO: Add more states to defenders");
+    console.log("TODO: Improve link miner efficiency");
+    console.log("TODO: Add states to defenders");
     console.log("TODO: Add ability for defenders to move on ramparts only");
     console.log("TODO: Add healers");
     console.log("TODO: Add wall repairers");
