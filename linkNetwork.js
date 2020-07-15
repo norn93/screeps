d57f25f0f4699a1f6296818917ec643d657aebf6
George Knowlden <george.knowlden@gmail.com>
2020-07-15 17:44:48 +0800

    Printing links

--- linkNetwork.js
+++ linkNetwork.js
@@ -1,5 +1,18 @@
 function linkNetwork() {
     console.log("Running link network...");
+
+    var links = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+            return (
+                structure.structureType == STRUCTURE_LINK
+            );
+        }
+    });
+
+    for (var i in links) {
+        var link = links[i];
+        console.log(link);
+    }
 }
 
 module.exports = linkNetwork;
