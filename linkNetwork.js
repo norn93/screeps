b63ccd50d7e437459098d879500e8ab3acca14d0
George Knowlden <george.knowlden@gmail.com>
2020-07-15 18:06:33 +0800

    Testing distance to source for links

--- linkNetwork.js
+++ linkNetwork.js
@@ -12,6 +12,17 @@ function linkNetwork() {
     for (var i in links) {
         var link = links[i];
         console.log(link);
+
+        // Now, we can programatically work out what kind of link this is
+        // If the link is close to a source, then it's job is to forward to the other link/s
+        // that are not near sources i.e. the link is a sender.
+
+        // If the link is not close to a source, then it must be a reciever.
+        // Recievers don't really need to do anything
+
+        var distance_to_source = link.pos.findClosestByRange(FIND_SOURCES);
+
+        console.log("Distance to source:", distance_to_source);
     }
 }
 
