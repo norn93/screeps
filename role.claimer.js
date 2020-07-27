22d9795a80dcdd0142a995a9ff1a7bd0911b9d1b
George Knowlden <george.knowlden@gmail.com>
2020-07-27 18:10:05 +0800

    Debugging movement

--- role.claimer.js
+++ role.claimer.js
@@ -2,6 +2,8 @@ var roleClaimer = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+
+        creep.say("?");
         
         var flags = Game.flags;
 
@@ -9,7 +11,9 @@ var roleClaimer = {
             var flag = flags[i];
 
             if (flag.room) {
+                console.log("Flag has a room");
                 if (creep.claimController(flag.room.controller) == ERR_NOT_IN_RANGE) {
+                    console.log("Moving toeards flag");
                     creep.moveByPath(flag.memory.path_from_spawn);
                 }
             } else {
