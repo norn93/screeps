b7c665fbcc204be3fa1d17f44bd29c06947546b5
George Knowlden <george.knowlden@gmail.com>
2020-07-29 22:54:43 +0800

    Reduce upgrader CPU

--- role.upgrader.js
+++ role.upgrader.js
@@ -3,21 +3,25 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function(creep, spawn) {
 
-        if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+        if (creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.upgrading = false;
         }
-        if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+        if (!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
             creep.memory.upgrading = true;
         }
 
-        if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+        if (creep.memory.upgrading) {
+            if (creep.pos.isNearTo(creep.room.controller)) {
+                creep.upgradeController(creep.room.controller);
+            } else {
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
             }
         }
         else {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            if (creep.pos.isNearTo(sources[0])) {
+                creep.harvest(sources[0]);
+            } else {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
