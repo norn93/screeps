8352e6794588f36509a5d8813ff3c932d6f60dab
George Knowlden <george.knowlden@gmail.com>
2020-08-02 17:15:16 +0800

    Test new spawning system logs

--- spawnCreep.js
+++ spawnCreep.js
@@ -2,6 +2,26 @@ const LOG = true;
 
 function spawnCreep(spawn, role, work=2, carry=1, move=1, attack=0, tough=0, claim=0) {
     
+    // New spawning system based on the energy we have
+    var energy = spawn.room.energyAvailable;
+    const cost = {"work": 200, "carry": 50, "move:", 50};
+    if (role == "builder" || role == "harvester") {
+        const recipe = {"work": 3, "carry": 1, "move:", 2};
+        var variable_cost = 0;
+        for (var i in recipe) {
+            variable_cost += recipe[i] * cost[i];
+        }
+        console.log("Variable cost:", variable_cost);
+
+        var x = energy/variable_cost;
+        console.log("Build factor:", x);
+
+        console.log("Recipe:");
+        for (var i in recipe) {
+            console.log(recipe[i] * x, "of", i);
+        }
+    }
+
     var roles = ["builder", "harvester", "freight", "upgrader", "defender", "linkminer", "linkupgrader", "claimer"];
     var valid_role = roles.includes(role);
     
